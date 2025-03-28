d8a694cd42d188405b980c6e9771f79ea9adab36
NeOpoku <imanimoss17@gmail.com>
2025-03-28 12:07:51 -0400

    modified:   index.html

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,8 +1,8 @@
 import {useEffect, useState} from "react";  
 
 
-export const LoadingScreen = () => {
-   const [text, setText] = useState("Loading");
+export const LoadingScreen = ({ onComplete }) => {
+   const [text, setText] = useState("");
    const fullText = "<Welcome To My Portfolio... />";
 
    useEffect(() => {
@@ -13,16 +13,21 @@ export const LoadingScreen = () => {
 
         if (index === fullText.length) {
             clearInterval(interval);
-        }
+
+            setTimeout(() => {
+                onComplete();
+        }, 1000);
+       }
     }, 100);
 
     return () => clearInterval(interval);
-});
+}, [onComplete]);
+
 
     return (
     <div className="fixed insert-0 z-50 bg-cream text-brown-100 flex-col items-center justify-center">
         <div className="mb-4 text-4xl font-mono font-bold">
-            Welcome To My Portfolio... <span className=" animate-blink ml-1"></span>
+            {text} <span className=" animate-blink ml-1"></span>
             </div>
 
             <div className="w- [200px] h-[2px} bg-brown-800 rounded relative overflow-hidden"></div>
